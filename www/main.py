9dc3182c10915a0cb81c12271d56dbf6fa87d1b7
palakiNarayan <148523736+palakiNarayan@users.noreply.github.com>
2024-07-01 23:12:04 +0530

    Add files via upload

---
+++ www/main.py
@@ -0,0 +1,23 @@
+from math import e
+import os
+import eel
+from engine.features import *
+from engine.command import *
+
+
+def start():
+
+    eel.init("www")
+
+    from engine.features import playAssistantSound
+
+    playAssistantSound()
+    
+    from engine.features import greed
+    greed()
+    
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html', mode=None, host='localhost', block=True)
+
+
